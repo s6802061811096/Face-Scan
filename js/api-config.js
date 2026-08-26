254fc1ac817b482ff11a395562c8d62edf4b8c1d
s6802061811096 <s6802061811096@email.kmutnb.ac.th>
2026-08-26 21:38:50 +0700

    Create api-config.js

---
+++ js/api-config.js
@@ -0,0 +1,2 @@
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbythgnfH_ABB-fxOKE1xTdFD3442irwRR9kqN4uHumQLT-A2EginjDrkomNP1nBtv_7jw/exec";
+localStorage.setItem('gasApiUrl', GAS_API_URL);
