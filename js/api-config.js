d1a770feaa79b7e8f1cc9b33044fb7cdb7f58600
s6802061811096 <s6802061811096@email.kmutnb.ac.th>
2026-08-27 09:11:14 +0700

    Update api-config.js

--- js/api-config.js
+++ js/api-config.js
@@ -1,2 +1,2 @@
-const GAS_API_URL = "https://script.google.com/macros/s/AKfycbythgnfH_ABB-fxOKE1xTdFD3442irwRR9kqN4uHumQLT-A2EginjDrkomNP1nBtv_7jw/exec";
+const GAS_API_URL = "https://script.google.com/macros/s/AKfycbyDVGn56wGTt0gEX8MLPuUH2aEi8d7r56NGqRcLgOtm938AzGY6h-oUmEOS2-JLcqLM/exec";
 localStorage.setItem('gasApiUrl', GAS_API_URL);
